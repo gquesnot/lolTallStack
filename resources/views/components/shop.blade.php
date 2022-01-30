f26936dc5632954ab302e6f18c2a12b061445089
gquesnot <gaqu1994@gmail.com>
2022-01-31 00:37:16 +0100

    close popup on click background

--- resources/views/components/shop.blade.php
+++ resources/views/components/shop.blade.php
@@ -49,7 +49,7 @@
             <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
                 <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-                     aria-hidden="true"></div>
+                     aria-hidden="true"@click="openModal = false"></div>
 
                 <!-- This element is to trick the browser into centering the modal contents. -->
                 <span class="hidden sm:inline-block sm:align-middle sm:h-screen"
@@ -130,20 +130,16 @@
                         </div>
 
                     </div>
-                    <div class="mt-5 sm:mt-6 flex">
+                    <div class="mt-5 sm:mt-6 flex justify-around">
                         <button type="button" @click="resetItems"
                                 class="mt-3 mx-3 w-1/3 inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm">
                             reset
                         </button>
-                        <button type="button" @click="openModal = false"
-                                class="mt-3 mx-3 w-1/3 inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm">
 
-                            Close
-                        </button>
                         <button type="button" @click="openModal = false"
                                 class="w-1/3 mx-3 inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm">
 
-                            Save
+                            Close
                         </button>
                     </div>
                 </div>
